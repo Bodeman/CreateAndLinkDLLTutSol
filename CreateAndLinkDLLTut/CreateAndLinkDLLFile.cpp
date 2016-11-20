09daf1859f267896ddbbb0cdc997820e2aadab2b
Benjamin W <XenoEgger@gmail.com>
2016-11-20 18:08:58 +0100

    Fixes string parameter method.

--- CreateAndLinkDLLTut/CreateAndLinkDLLFile.cpp
+++ CreateAndLinkDLLTut/CreateAndLinkDLLFile.cpp
@@ -7,3 +7,11 @@ float DLL_EXPORT getCircleArea(float radius)
 {
 	return 3.1416f * (radius * radius);
 }
+
+std::string DLL_EXPORT getCircleString(std::string baseString)
+{
+	std::string additionalString = std::string("AdditionalStringFromDLL");
+	baseString = std::string(baseString + additionalString);
+
+	return baseString;
+}
--- CreateAndLinkDLLTut/CreateAndLinkDLLFile.h
+++ CreateAndLinkDLLTut/CreateAndLinkDLLFile.h
@@ -1,5 +1,7 @@
 #pragma once  
 
+#include <string>
+
 #define DLL_EXPORT __declspec(dllexport)
 
 #ifdef __cplusplus
@@ -8,6 +10,7 @@ extern "C"
 #endif
 
 	float DLL_EXPORT getCircleArea(float radius);
+	std::string DLL_EXPORT getCircleString(std::string baseString);
 
 #ifdef __cplusplus
 }

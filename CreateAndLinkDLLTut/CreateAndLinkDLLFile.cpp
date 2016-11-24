b4815a7082e9b208ffec45c25899e0b4747e56e5
Benjamin W <XenoEgger@gmail.com>
2016-11-24 18:57:31 +0100

    Refactors & adds comments.

--- CreateAndLinkDLLTut/CreateAndLinkDLLFile.cpp
+++ CreateAndLinkDLLTut/CreateAndLinkDLLFile.cpp
@@ -2,15 +2,16 @@
 
 #include "CreateAndLinkDLLFile.h"
 
-// a sample exported function
+//Exported method that calculates the are of a circle by a given radius
 float DLL_EXPORT getCircleArea(float radius)
 {
 	return float(3.1416f * (radius * radius));
 }
 
-std::string DLL_EXPORT getCircleString(std::string baseString)
+//Exported method that adds a string to a given string
+std::string DLL_EXPORT getAdditionalString(std::string baseString)
 {
-	std::string additionalString = "AdditionalStringFromDLL";
+	std::string additionalString = " + AdditionalStringFromDLL";
 	baseString = baseString + additionalString;
 
 	return std::string(baseString);
--- CreateAndLinkDLLTut/CreateAndLinkDLLFile.h
+++ CreateAndLinkDLLTut/CreateAndLinkDLLFile.h
@@ -1,16 +1,16 @@
 #pragma once  
 
-#include <string>
+#include <string>	//needed for std::string
 
-#define DLL_EXPORT __declspec(dllexport)
+#define DLL_EXPORT __declspec(dllexport)	//shortens __declspec(dllexport) to DLL_EXPORT
 
-#ifdef __cplusplus
+#ifdef __cplusplus		//if C++ is used convert it to C to prevent C++'s name mangling of method names
 extern "C"
 {
 #endif
 
 	float DLL_EXPORT getCircleArea(float radius);
-	std::string DLL_EXPORT getCircleString(std::string baseString);
+	std::string DLL_EXPORT getAdditionalString(std::string baseString);
 
 #ifdef __cplusplus
 }
